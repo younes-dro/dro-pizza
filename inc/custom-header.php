4bb3506725c610624ce7f81b6797e43c0521b062
younes-dro <dro66_8@hotmail.fr>
2019-08-31 04:49:25 +0100

    Define new size for image header

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -20,8 +20,8 @@ function dro_pizza_custom_header_setup() {
 	add_theme_support( 'custom-header', apply_filters( 'dro_pizza_custom_header_args', array(
 		'default-image'          => '',
 		'default-text-color'     => '000000',
-		'width'                  => 1000,
-		'height'                 => 250,
+		'width'                  => 1500,
+		'height'                 => 700,
 		'flex-height'            => true,
 		'wp-head-callback'       => 'dro_pizza_header_style',
 	) ) );
