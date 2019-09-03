142cc901ec7473f2b024d67b13a1613a943fdfa1
younes-dro <dro66_8@hotmail.fr>
2019-09-03 04:34:45 +0100

    Remove the Word Tagged ,  add space before the comma (list of categories)

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -58,18 +58,18 @@ if ( ! function_exists( 'dro_pizza_entry_footer' ) ) :
 	function dro_pizza_entry_footer() {
 		// Hide category and tag text for pages.
 		if ( 'post' === get_post_type() ) {
-			/* translators: used between list items, there is a space after the comma */
-			$categories_list = get_the_category_list( esc_html__( ', ', 'dro-pizza' ) );
+			/* translators: used between list items, there is a space before and after the comma */
+			$categories_list = get_the_category_list( esc_html__( ' , ', 'dro-pizza' ) );
 			if ( $categories_list ) {
 				/* translators: 1: list of categories. */
 				printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', 'dro-pizza' ) . '</span>', $categories_list ); // WPCS: XSS OK.
 			}
 
 			/* translators: used between list items, there is a space after the comma */
-			$tags_list = get_the_tag_list( '', esc_html_x( ', ', 'list item separator', 'dro-pizza' ) );
+			$tags_list = get_the_tag_list( );
 			if ( $tags_list ) {
 				/* translators: 1: list of tags. */
-				printf( '<span class="tags-links">' . esc_html__( 'Tagged %1$s', 'dro-pizza' ) . '</span>', $tags_list ); // WPCS: XSS OK.
+				printf( '<span class="tags-links">' . esc_html__( '%1$s', 'dro-pizza' ) . '</span>', $tags_list ); // WPCS: XSS OK.
 			}
 		}
 
