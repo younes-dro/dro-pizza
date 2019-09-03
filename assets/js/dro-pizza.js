88ac102afc84685d28246523bb276c570d648cb8
younes-dro <dro66_8@hotmail.fr>
2019-09-03 16:46:27 +0100

    Toggleclass open for tne entry footer element to handle the position from absolute/initial

--- assets/js/dro-pizza.js
+++ assets/js/dro-pizza.js
@@ -17,8 +17,10 @@
      * Show/ hide meta entry (Tags , Categories).
      */
     $(".open-meta-tags").on('click', function () {
+        $entryfooter = $(this).parent(); 
         $(this).parent().parent().toggleClass('active').promise().done(function () {
             $(this).find('.meta-infos').slideToggle('fast');
+            $entryfooter.toggleClass('open');
         });
         $(this).toggleClass('active');
     });
