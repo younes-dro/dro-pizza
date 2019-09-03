c97bea2973b5adb112ace6370492506a2d4a05e4
younes-dro <dro66_8@hotmail.fr>
2019-09-03 04:31:36 +0100

    Dispplay the content of the single

---
+++ template-parts/content-post.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Dro_Pizza
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> style="background-color: #00435d">
+	<header class="entry-header">
+		<?php
+		if ( is_singular() ) :
+			the_title( '<h1 class="entry-title">', '</h1>' );
+		else :
+			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+		endif;
+
+		if ( 'post' === get_post_type() ) :
+			?>
+			<div class="entry-meta">
+				<?php
+				dro_pizza_posted_on();
+				dro_pizza_posted_by();
+				?>
+			</div><!-- .entry-meta -->
+		<?php endif; ?>
+	</header><!-- .entry-header -->
+
+	<?php dro_pizza_post_thumbnail(); ?>
+
+	<div class="entry-content">
+		<?php the_content()  ?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php dro_pizza_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-<?php the_ID(); ?> -->
+</div><!-- .col-12 col-md-6 col-lg-4 -->
