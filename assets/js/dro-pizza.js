8f8e385f437ad17d9020572beaedea93fa7b354a
younes-dro <dro66_8@hotmail.fr>
2019-09-03 04:32:12 +0100

    Add js function to show/hide meta entry

--- assets/js/dro-pizza.js
+++ assets/js/dro-pizza.js
@@ -4,14 +4,24 @@
  * 
  */
 
-(function($){
+(function ($) {
     /**
-     * Creating the mobile menu
+     * Creating the mobile menu.
      * 
      */
-        var droPizzaMainMenu = $('nav.main-navigation ul.nav-menu').clone(true, true);
+    var droPizzaMainMenu = $('nav.main-navigation ul.nav-menu').clone(true, true);
     console.log(droPizzaMainMenu);
-    $(droPizzaMainMenu).droSlidingMenu();    
+    $(droPizzaMainMenu).droSlidingMenu();
+
+    /**
+     * Show/ hide meta entry (Tags , Categories).
+     */
+    $(".open-meta-tags").on('click', function () {
+        $(this).parent().parent().toggleClass('active').promise().done(function () {
+            $(this).find('.meta-infos').slideToggle('fast');
+        });
+        $(this).toggleClass('active');
+    });
 })(jQuery);
 
 
