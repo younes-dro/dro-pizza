5edf8871e6ed51e94c251ec51f57a981a8808bb4
younes-dro <dro66_8@hotmail.fr>
2019-09-04 05:37:02 +0100

    Change the logo height to 100px

--- functions.php
+++ functions.php
@@ -74,8 +74,8 @@ if ( ! function_exists( 'dro_pizza_setup' ) ) :
 		 * @link https://codex.wordpress.org/Theme_Logo
 		 */
 		add_theme_support( 'custom-logo', array(
-			'height'      => 250,
 			'width'       => 250,
+                        'height'      => 100,
 			'flex-width'  => true,
 			'flex-height' => true,
 		) );
