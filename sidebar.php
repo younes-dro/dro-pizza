91b9ecfa32d033dec865920f3dc85bb6825c018a
younes-dro <dro66_8@hotmail.fr>
2019-09-05 06:22:41 +0100

    Style the widgets

--- sidebar.php
+++ sidebar.php
@@ -11,7 +11,7 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 	return;
 }
 ?>
-<div class="col-xl-3">
+<div class="col-12 col-xl-3">
 <aside id="secondary" class="widget-area">
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
 </aside><!-- #secondary -->
