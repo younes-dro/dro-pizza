9a7fdbcfc157099f1e072e2fd4315496e18784cb
younes-dro <dro66_8@hotmail.fr>
2019-09-07 03:16:03 +0100

    Test the new customizer footer options

--- footer.php
+++ footer.php
@@ -9,24 +9,41 @@
  * @package Dro_Pizza
  */
 
+        $display_footer_logo = get_theme_mod('dro_pizza_footer_logo');
+        $display_footer_blogname = get_theme_mod('dro_pizza_footer_blogname');
+        $copyright_link = get_theme_mod('dro_pizza_copyright_link');
+        $copyright_text = get_theme_mod('dro_pizza_copyright_text');
+
 ?>
 
 	</div><!-- #content / .row -->
 
 	<footer id="colophon" class="site-footer row">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'dro-pizza' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'dro-pizza' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'dro-pizza' ), 'dro-pizza', '<a href="https://dro.123.fr/">Younes DRO</a>' );
-				?>
-		</div><!-- .site-info -->
+            <?php if($display_footer_blogname): ?>
+                <div class="col-12 blog-name-footer">
+                    <h3><?php bloginfo('blog_name')?></h3>
+                </div>            
+            <?php endif;?>
+            
+            <div class="site-info col-12">
+                <?php 
+                    if($display_footer_logo):
+                        the_custom_logo();
+                    endif;
+                ?>
+                <?php 
+                if(!empty($copyright_link)): ?>
+                &copy; <a href="<?php echo esc_url($copyright_link)?>" title="<?php echo esc_attr($copyright_text)?>">
+                    <?php esc_html_e($copyright_text ) ?>
+                    <a>
+                <?php endif; ?>
+		<!--<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'dro-pizza' ) ); ?>">-->
+                    <?php
+			/* translators: %s: CMS name, i.e. WordPress. */
+//			printf( esc_html__( 'Proudly powered by %s', 'dro-pizza' ), 'WordPress' );
+                    ?>
+		<!--</a>-->
+            </div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- .container-fluid -->
 </div><!-- #page -->
