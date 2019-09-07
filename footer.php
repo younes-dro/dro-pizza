a078ff68a110c1aeb77c23cabf91d19b15b36072
younes-dro <dro66_8@hotmail.fr>
2019-09-07 19:07:23 +0100

    Handle the cases of copyright text and link if not empty

--- footer.php
+++ footer.php
@@ -9,10 +9,10 @@
  * @package Dro_Pizza
  */
 
-        $display_footer_logo = get_theme_mod('dro_pizza_footer_logo');
-        $display_footer_blogname = get_theme_mod('dro_pizza_footer_blogname');
-        $copyright_link = get_theme_mod('dro_pizza_copyright_link');
-        $copyright_text = get_theme_mod('dro_pizza_copyright_text');
+        $display_footer_logo = get_theme_mod( 'dro_pizza_footer_logo' );
+        $display_footer_blogname = get_theme_mod( 'dro_pizza_footer_blogname' );
+        $copyright_link = get_theme_mod( 'dro_pizza_copyright_link' );
+        $copyright_text = get_theme_mod( 'dro_pizza_copyright_text' );
 
 ?>
 
@@ -21,28 +21,28 @@
 	<footer id="colophon" class="site-footer row">
             <?php if($display_footer_blogname): ?>
                 <div class="col-12 blog-name-footer">
-                    <h3><?php bloginfo('blog_name')?></h3>
+                    <h3><?php bloginfo( 'blog_name' )?></h3>
                 </div>            
             <?php endif;?>
             
             <div class="site-info col-12">
                 <?php 
-                    if($display_footer_logo):
+                    if( $display_footer_logo ):
                         the_custom_logo();
                     endif;
                 ?>
                 <?php 
-                if(!empty($copyright_link)): ?>
-                &copy; <a href="<?php echo esc_url($copyright_link)?>" title="<?php echo esc_attr($copyright_text)?>">
-                    <?php esc_html_e($copyright_text ) ?>
-                    <a>
+                if( ! empty( $copyright_link ) && ! empty( $copyright_text )): ?>
+                &copy; <a href="<?php echo esc_url( $copyright_link )?>" title="<?php echo esc_attr( $copyright_text )?>">
+                    <?php echo esc_html( $copyright_text ) ?>
+                    </a>
+                <?php elseif ( ! empty( $copyright_link ) ): ?>
+                &copy; <a href="<?php echo esc_url( $copyright_link )?>" title="<?php echo esc_attr( $copyright_link ) ?>">
+                    <?php echo esc_html( $copyright_link ) ?>
+                </a>
+                <?php elseif( ! empty( $copyright_text ) ): ?>
+               &copy; <?php  echo esc_html( $copyright_text ); ?>
                 <?php endif; ?>
-		<!--<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'dro-pizza' ) ); ?>">-->
-                    <?php
-			/* translators: %s: CMS name, i.e. WordPress. */
-//			printf( esc_html__( 'Proudly powered by %s', 'dro-pizza' ), 'WordPress' );
-                    ?>
-		<!--</a>-->
             </div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- .container-fluid -->
