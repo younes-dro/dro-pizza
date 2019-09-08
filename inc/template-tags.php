f11e7e910a696d9ee34b5403d176f31b186974d5
younes-dro <dro66_8@hotmail.fr>
2019-09-08 05:28:32 +0100

    Remove the On and By from meta entry

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -26,7 +26,7 @@ if ( ! function_exists( 'dro_pizza_posted_on' ) ) :
 
 		$posted_on = sprintf(
 			/* translators: %s: post date. */
-			esc_html_x( 'on %s', 'post date', 'dro-pizza' ),
+			esc_html_x( '%s', 'post date', 'dro-pizza' ),
 			'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
 		);
 
@@ -42,7 +42,7 @@ if ( ! function_exists( 'dro_pizza_posted_by' ) ) :
 	function dro_pizza_posted_by() {
 		$byline = sprintf(
 			/* translators: %s: post author. */
-			esc_html_x( 'by %s', 'post author', 'dro-pizza' ),
+			esc_html_x( '%s', 'post author', 'dro-pizza' ),
 			'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
 		);
 
