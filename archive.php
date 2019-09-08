1a28715c3be7b01d99bd84837f37e92d0875426d
younes-dro <dro66_8@hotmail.fr>
2019-09-08 04:25:40 +0100

    Style accomplish for archive pages

--- archive.php
+++ archive.php
@@ -9,13 +9,20 @@
 
 get_header();
 ?>
-
+<?php
+    if(dro_pizza_sidebar_status( 'sidebar-1' )){
+            $col_count = 'col-12 col-xl-9';
+        }else{
+            $col_count = 'col-lg-12';
+        }
+?>
+<div class="<?php echo $col_count?>">
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+		<main id="main" class="site-main row justify-content-center">
 
 		<?php if ( have_posts() ) : ?>
 
-			<header class="page-header">
+			<header class="page-header col-12">
 				<?php
 				the_archive_title( '<h1 class="page-title">', '</h1>' );
 				the_archive_description( '<div class="archive-description">', '</div>' );
@@ -32,11 +39,11 @@ get_header();
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
 				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+				get_template_part( 'template-parts/content', 'home' );
 
 			endwhile;
 
-			the_posts_navigation();
+			dro_pizza_posts_pagination();
 
 		else :
 
@@ -47,7 +54,7 @@ get_header();
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
-
+</div><!-- .col- -->
 <?php
 get_sidebar();
 get_footer();
